736acdeb805fcf0c92f881ba6f064d5f29f036c7
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2024-12-17 20:05:18 +0200

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/bgimages ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/bspwm ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/calcurse ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/kitty ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/polybar ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/rofi ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/sxhkd ~/.config/
-cp -r ~/BSPWM-Dracula-Mint/Dracula-Mint/xcolor-pick ~/.config/
+# Устанавливаем все программы
+sudo pacman -S kitty bspwm sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi firefox feh ttf-jetbrains-mono ttf-firacode-nerd neofetch micro zenity calcurse flameshot mc dunst xcolor --noconfirm
 
+# Копируем конфигурационные файлы после установки ПО
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/bgimages ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/bspwm ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/calcurse ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/kitty ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/polybar ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/rofi ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/sxhkd ~/.config/
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/xcolor-pick ~/.config/
+
+# Копируем .xinitrc в домашнюю директорию
 cp -f ~/BSPWM-Dracula-Mint/.xinitrc ~/
 
-
+# Настраиваем права на выполнение
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/rofi/network-manager.sh
 chmod +x ~/.config/rofi/rofi-power-menu.sh
 chmod +x ~/.config/sxhkd/sxhkdrc
 chmod +x ~/.config/xcolor-pick/xcolor-pick.sh
-
-
-sudo pacman -S kitty bspwm sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi firefox feh ttf-jetbrains-mono ttf-firacode-nerd neofetch micro zenity calcurse flameshot mc dunst xcolor --noconfirm
