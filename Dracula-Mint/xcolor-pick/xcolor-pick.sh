aec778cf4a9be6c4cfc372147541d01c49c1bc1e
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2024-12-17 18:36:22 +0200

    Add files via upload

--- Dracula-Mint/xcolor-pick/xcolor-pick.sh
+++ Dracula-Mint/xcolor-pick/xcolor-pick.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
 
+#   ██╗  ██╗ ██████╗ ██████╗ ██╗      ██████╗ ██████╗ 
+#   ╚██╗██╔╝██╔════╝██╔═══██╗██║     ██╔═══██╗██╔══██╗
+#    ╚███╔╝ ██║     ██║   ██║██║     ██║   ██║██████╔╝
+#    ██╔██╗ ██║     ██║   ██║██║     ██║   ██║██╔══██╗
+#   ██╔╝ ██╗╚██████╗╚██████╔╝███████╗╚██████╔╝██║  ██║
+#   ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+# color picker for X.
+# Simple Script To Pick Color Quickly Using Gpick.
+# Created By: https://github.com/rxyhn 
+
+
+COLOR=$(xcolor) || { echo "er"; exit 1; }
+
+notify-send "Your color" $COLOR -h string:bgcolor:$COLOR
