536568e17a514351ad59faec8240e7f1cb7c520d
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2024-12-18 13:48:09 +0200

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Устанавливаем все программы
-sudo pacman -S kitty bspwm pulseaudio sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi firefox feh ttf-jetbrains-mono ttf-firacode-nerd neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor --noconfirm
+sudo pacman -S kitty bspwm pulseaudio nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi firefox feh ttf-jetbrains-mono ttf-firacode-nerd neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor --noconfirm
 
 #Создание папки .config
 mkdir -p ~/.config
