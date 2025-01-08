a4a1fbc328f3c8c9978a0647a82b04c039c59aee
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-01-08 16:36:15 +0200

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -3,7 +3,7 @@
 # Устанавливаем все программы
 sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi firefox feh ttf-jetbrains-mono ttf-firacode-nerd neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor pamixer --noconfirm
 
-#Создание папки .config
+#Создание папки .config и .themes
 mkdir -p ~/.config
 mkdir -p ~/.themes
 
@@ -20,6 +20,10 @@ cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/gtk-3.0 ~/.config/
 
 # Копируем .xinitrc в домашнюю директорию
 cp -f ~/BSPWM-Dracula-Mint/.xinitrc ~/
+
+#Добавление GTK темы
+tar -xf ~/BSPWM-Dracula-Mint/Dracula-Mint/Gradient-Dark-GTK.tar.gz
+cp -rf Gradient-Dark-GTK ~/.themes/
 
 # Настраиваем права на выполнение
 chmod +x ~/.config/bspwm/bspwmrc
