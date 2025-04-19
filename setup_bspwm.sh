e86914b7c58bf37f10806728a185d2b77b494212
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-04-19 12:47:34 +0300

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Устанавливаем все программы
-sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi rofi-calc rofi-emoji firefox feh ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji xclip neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor pamixer udiskie vlc eog xarchiver redshift telegram-desktop noisetorch pavucontrol unzip --noconfirm
+sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi rofi-calc rofi-emoji firefox feh ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji xclip neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor pamixer udiskie vlc eog xarchiver redshift telegram-desktop pavucontrol unzip --noconfirm
 
 #Создание папки .config и .themes
 mkdir -p ~/.config
@@ -44,6 +44,7 @@ cd ~/yay
 makepkg -si
 yay -S bibata-cursor-theme-bin
 yay -S xkblayout-state
+yay -S noisetorch
 yay -S discord_arch_electron
 
 # Настраиваем права на выполнение
