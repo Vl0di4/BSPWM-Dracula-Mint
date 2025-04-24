ce46fac43701070a84bc45ee5bf15f9044c0f618
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-04-24 06:36:51 +0300

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -5,6 +5,8 @@ sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom p
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 rm ~/.zshrc
 cp ~/BSPWM-Dracula-Mint/Dracula-Mint/.zshrc ~/
