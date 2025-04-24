2b91ed7f3bd95476890c8f5cca11c76a7c960d56
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-04-24 06:43:11 +0300

    Update setup_bspwm.sh

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Устанавливаем все программы
-sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi rofi-calc rofi-emoji firefox feh ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji xclip neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor pamixer udiskie vlc eog xarchiver redshift telegram-desktop pavucontrol unzip zsh eza flatpak --noconfirm
+sudo pacman -S kitty bspwm nemo sxhkd xorg xorg-server xorg-xinit neovim picom polybar python3 python-pywal nitrogen rofi rofi-calc rofi-emoji firefox feh ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji xclip neofetch micro zenity calcurse flameshot mc dunst libnotify xcolor pamixer udiskie vlc eog xarchiver redshift telegram-desktop pavucontrol unzip zsh eza flatpak btop --noconfirm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
